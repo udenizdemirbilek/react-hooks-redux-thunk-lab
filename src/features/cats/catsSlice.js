29e4c8130b373fa994b951b24c5e17cd90f546fa
demirbileku <udemirbilek2@gmail.com>
2021-07-24 16:53:09 +0300

    commit tree 8ce67e15dab17b405d5f006d9881ec0a5a1b04fb

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,36 @@
-export function fetchCats() {}
+export function fetchCats() {
+    return function (dispatch) {
+        dispatch({ type: "cats/catsLoading" });
+        fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+          .then((response) => response.json())
+          .then((cats) =>
+            dispatch({
+              type: "cats/catsLoaded",
+              payload: cats.images,
+            })
+          );
+      };
+}
 
-export default function catsReducer() {}
+const initialState = {
+    entities: [], //array of cats
+    status: "idle", //loading status for fetch
+  };
+
+export default function catsReducer(state = initialState, action) {
+  switch (action.type) {
+    case "cats/catsLoaded":
+      return {
+        ...state,
+        status: "idle",
+        entities: action.payload,
+      };
+    case "cats/catsLoading":
+      return {
+        ...state,
+        status: "loading",
+      };
+    default:
+      return state;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { applyMiddleware, createStore } from "redux";
+import thunkMiddleware  from "redux-thunk";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import catsReducer from "./features/cats/catsSlice";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+const store = createStore(
+    catsReducer,
+    composedEnhancer
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root"));
